4ac806935f311e298cc3e3778c49ce4db388242a
Prathis Upadhyay <prithuupadhyay0123@gmail.com>
2022-03-11 00:47:19 +0530

    my inital commit

--- FileSharing/settings.py
+++ FileSharing/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = ')+xr!@#&%(w-f&e=(sii7q$(f7gg9zz0g+b4^-btdi4&ud^vxf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- FileSharing/urls.py
+++ FileSharing/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
-
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$'), serve, {'document_root' : settings.MEDIA_ROOT}
 
 ]
 
