4cf0b9a5b6331ff090e8096176f9890a561e27fd
Prathis Upadhyay <prithuupadhyay0123@gmail.com>
2022-03-11 00:49:58 +0530

    my inital commit

--- FileSharing/urls.py
+++ FileSharing/urls.py
@@ -22,8 +22,7 @@ from django.views.static import serve
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
-    re_path(r'^media/(?P<path>.*)$'), serve, {'document_root' : settings.MEDIA_ROOT}
-
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root' : settings.MEDIA_ROOT}),
 ]
 
 
